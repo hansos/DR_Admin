43ec7becaea5122265d01998e71b80f3cdb615bf
hansos <hans.olav@sorteberg.com>
2026-01-22 12:57:22 +0100

    Add IEmailSender interface with async email methods
    Created the IEmailSender interface in the EmailSenderLib.Interfaces namespace. It provides two async methods: one for sending basic emails and another for sending emails with attachments, both supporting optional HTML formatting. Added necessary using directives.

---
+++ EmailSenderLib/Interfaces/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSenderLib.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string to, string subject, string body, bool isHtml = false);
+        Task SendEmailAsync(string to, string subject, string body, List<string> attachments, bool isHtml = false);
+
+    }
+}
